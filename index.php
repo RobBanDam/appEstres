d639d81896e2e2b63a43421abf6951ff4a369f2a
RobBanDam <robert.mtz.a.08@gmail.com>
2024-02-08 01:59:22 -0600

    08/02/2024 Interfaz de la WebApp

---
+++ index.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Title</title>
+	<!-- Required meta tags -->
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+	<!-- Bootstrap CSS v5.2.1 -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+	<main>
+		<div class="container">
+			<br>
+			<div class="row">
+				<div class="col">
+					<h4>TEST DE ESTRÉS LABORAL</h4>
+					Permite conocer en qué grado el trabajador padece los síntomas asociados al estrés.
+
+					<br><br>
+					<strong>Instrucciones:</strong>
+					De los siguientes síntomas, selecciona el grado experimentado durante los
+					últimos 3 meses de acuerdo al semáforo presentado.
+					<br>
+					<span class="badge color1">1 (Nunca )</span>
+					<span class="badge color2">2 (Casi Nunca )</span>
+					<span class="badge color3">3 (Pocas Veces )</span>
+					<span class="badge color4">4 (Algunas Veces )</span>
+					<span class="badge color5">5 (Relativamente Frecuente )</span>
+					<span class="badge color6">6 (Muy Frecuente )</span>
+
+					<br>
+					<br>
+
+					<div class="card">
+						<div class="card-body">
+							1.- Imposibilidad de conciliar el sueño.
+							<br>
+							<span class="badge color1">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">1</label>
+								</div>
+							</span>
+
+							<span class="badge color2">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">2</label>
+								</div>
+							</span>
+
+							<span class="badge color3">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">3</label>
+								</div>
+							</span>
+
+							<span class="badge color4">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">4</label>
+								</div>
+							</span>
+
+							<span class="badge color5">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">5</label>
+								</div>
+							</span>
+
+							<span class="badge color6">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">6</label>
+								</div>
+							</span>
+
+							<br>
+							<br>
+							2.- Jaquecas y dolores de cabeza.
+							<br>
+							<span class="badge color1">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">1</label>
+								</div>
+							</span>
+
+							<span class="badge color2">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">2</label>
+								</div>
+							</span>
+
+							<span class="badge color3">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">3</label>
+								</div>
+							</span>
+
+							<span class="badge color4">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">4</label>
+								</div>
+							</span>
+
+							<span class="badge color5">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">5</label>
+								</div>
+							</span>
+
+							<span class="badge color6">
+								<div class="form-check form-check-inline">
+									<input class="form-check-input" type="radio" name="pregunta1" value="option1" />
+									<label class="form-check-label" for="">6</label>
+								</div>
+							</span>
+						</div>
+					</div>
+
+					<br>
+					<button type="button" class="btn btn-primary">
+						Enviar 
+					</button>
+
+				</div>
+
+				<div class="col">
+					<h4>Respuestas</h4>
+				</div>
+			</div>
+
+		</div>
+	</main>
+
+	<footer>
+		<!-- place footer here -->
+	</footer>
+	<!-- Bootstrap JavaScript Libraries -->
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
